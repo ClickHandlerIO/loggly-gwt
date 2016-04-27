29a4b0e9026269359e038017cdd1370d8e24d2be
Chris Jones <chris@axismedtech.com>
2016-04-27 09:31:16 -0700

    updates

--- src/main/java/io/clickhandler/logglyGwt/client/Loggly.java
+++ src/main/java/io/clickhandler/logglyGwt/client/Loggly.java
@@ -1,24 +1,16 @@
 package io.clickhandler.logglyGwt.client;
 
-import com.google.gwt.core.client.JavaScriptObject;
-import jsinterop.annotations.JsOverlay;
-import jsinterop.annotations.JsType;
-
-@JsType(isNative = true, namespace = "_LTracker")
 public class Loggly {
-    private static native void push(Object object);
 
-    @JsOverlay
     public static void push(String logLevel, String message, Object... data) {
-        push(wrap(logLevel, message, data));
+        pushNative(logLevel, message, data);
     }
 
-    @JsOverlay
-    private static native JavaScriptObject wrap(String logLevel, String message, Object... data) /*-{
-        return {
+    private static native void pushNative(String logLevel, String message, Object... data) /*-{
+        _LTracker.push({
             logLevel: logLevel,
             message: message,
             data: data
-        }
+        });
     }-*/;
 }
